289268e174f3d25b19b79af623a471d45b625246
Apphle <125525800+Apphle@users.noreply.github.com>
2023-03-08 11:11:14 +0900

    Add files via upload

---
+++ 20230308/20230308_1/20230308_1/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>  // header 파일 추가 standard 10
+
+
+int main(void)
+{
+
+	printf("test \n");  // \n line feed
+	printf("202315015 \n");  // \n line feed
+	printf("Jae Hyeok LEE \n");  // \n line feed
+
+	return 0;
+}
+
+//ctrl+f5 는 실행
