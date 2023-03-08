3138b96bf86216559935cb893febb546b015a082
Apphle <125525800+Apphle@users.noreply.github.com>
2023-03-08 11:14:46 +0900

    Add files via upload

---
+++ 20230308/20230308_8/20230308_8/main.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void)
+{
+	/*
+	printf("일  월  화  수  목  금  토\n");
+	printf("1   2   3   4   5   6   7\n");
+	printf("8   9   10  11  12  13  14\n");
+	printf("15  16  17  18  19  20  21\n");
+	printf("22  23  24  25  26  27  28\n");
+	printf("29  30  31\n");
+	*/
+
+	printf("일\t월\t화\t수\t목\t금\t토\n");
+	printf("1\t2\t3\t4\t5\t6\t7\n");
+	printf("8\t9\t10\t11\t12\t13\t14\n");
+	printf("15\t16\t17\t18\t19\t20\t21\n");
+	printf("22\t23\t24\t25\t26\t27\t28\n");
+	printf("29\t30\t31\n");
+
+	return 0;
+}
