44f8e239ef15d20c9749e2945410642690be569f
Apphle <125525800+Apphle@users.noreply.github.com>
2023-03-22 16:02:12 +0900

    Add files via upload

---
+++ 20230315/20230315_1/20230315_1/main.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(void)
+{
+	//printf("hello world\n\n\n\n");
+	//printf("한라대학교\n\n\n\n");
+	//printf("202315015 이재혁\n\n\n\n");
+
+	//printf("결과값 = %d\n", 2 + 3);
+	//printf("결과값 = %d\n", 2 - 3);
+	//printf("결과값 = %d\n", 2 * 3);
+	//printf("결과값 = %f\n", 2.0 / 3.0);
+
+	//printf("9 x 1 = %d\n", 9*1);
+	//printf("9 x 2 = %d\n", 9*2);
+	//printf("9 x 3 = %d\n", 9*3);
+	//printf("9 x 4 = %d\n", 9*4);
+	//printf("9 x 5 = %d\n", 9*5);
+
+	//int x = 5;
+	//int y = 3;
+
+	//printf("%d + %d = %d\n", x, y, x + y);
+	//printf("%d - %d = %d\n", x, y, x - y);
+	//printf("%d * %d = %d\n", x, y, x * y);
+	//printf("%d / %d = %d\n", x, y, x / y);                                                
+
+	return 0;
+}
