3e913202aa5b6318e91490b307b4ba1e23531791
Apphle <125525800+Apphle@users.noreply.github.com>
2023-04-10 20:49:12 +0900

    Add files via upload

---
+++ c언어 도전 150제/202315015_2/202315015_2/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+int main(void)
+{
+	// 상수형 배우기
+	double x = 1;
+	double z = 0;
+
+	z = x + M_PI;
+
+	printf(" x + y = %.6lf", z);
+
+}
