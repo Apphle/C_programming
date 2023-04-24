7a5b9ae871a0298886170bf3d481587addf9f37c
Apphle <125525800+Apphle@users.noreply.github.com>
2023-04-24 20:53:14 +0900

    Add files via upload

---
+++ 20230424/20230424_1/20230424_3/main.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int x1, x2, x3, x4, x5;
+
+	x1 = 0;
+	x2 = 1;
+
+	printf("몇 번째 항까지 구할까요? \n");
+	scanf("%d", &x4);
+
+	for (x5 = 0;x5 <= x4 ;x5++)
+	{
+		printf("%d\n",x1);
+		x3 = x1 + x2;
+		x1 = x2;
+		x2 = x3;
+
+	}
+}
