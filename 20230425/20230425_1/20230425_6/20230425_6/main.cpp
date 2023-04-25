fa895e6c8f4c316bdac524645b14cf7228cf5afa
Apphle <125525800+Apphle@users.noreply.github.com>
2023-04-25 21:53:31 +0900

    Add files via upload

---
+++ 20230425/20230425_1/20230425_6/20230425_6/main.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int score;
+
+	printf("성적을 입력하시오:");
+	scanf("%d", &score);
+
+	if (score >= 90)
+	{
+		printf("학점 A\n");
+	}
+	else if (score >= 80)
+	{
+		printf("학점 B\n");
+	}
+	else if (score >= 70)
+	{
+		printf("학점 C\n");
+	}
+	else if (score >= 60)
+	{
+		printf("학점 D\n");
+	}
+	else
+	{
+		printf("학점 F\n");
+	}
+	return 0;
+}
