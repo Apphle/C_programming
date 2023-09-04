0b03b023303cc1206df2dac33dfce3b99e6609c4
Apphle <125525800+Apphle@users.noreply.github.com>
2023-09-04 21:17:12 +0900

    Add files via upload

---
+++ 2학기/20230904/20230904_7/20230904_7/main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+struct Node 
+{
+	int a;
+	double b;
+};
+
+int main()
+{
+	struct Node* print;
+	FILE* fp;
+	fp = fopen("data.txt", "wb"); // 바이너리값으로 저장
+
+	print = (struct Node*)malloc(sizeof(struct Node));
+	print->a = 1;
+	print->b = 3.14;
+
+	fwrite((void*)print, sizeof(struct Node), 1, fp);
+	fclose(fp); // 파일 닫기
+	free(print); // 메모리반환
+}
